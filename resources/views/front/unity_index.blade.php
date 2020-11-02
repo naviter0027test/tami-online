597a2a207ae578b352570fb192feaeb97cd2dbaf
naviter0027test <naviter0027test@gmail.com>
2020-11-02 11:27:41 +0800

    2020-11-02 unity update

--- resources/views/front/unity_index.blade.php
+++ resources/views/front/unity_index.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="TemplateData/style.css">
     <script src="TemplateData/UnityProgress.js"></script>
     <script src="Build/UnityLoader.js"></script>
+	<script src="https://smtpjs.com/v3/smtp.js"></script>
 	<input id="locationHref"/>
     <script>
       var unityInstance = UnityLoader.instantiate("unityContainer", "Build/web.json", {onProgress: UnityProgress});
@@ -23,6 +24,27 @@
 				alert(url + "\nThe sharing link has been successfully copied");
 			else if(lanauge=="RU")
 				alert(url + "\nСсылка для совместного использования успешно скопирована");
+		}
+		function sendEmail(str)
+		{
+			var str = JSON.parse(str);
+			var _emailFrom = str.emailFrom;
+			var _emailSecureToken = str.emailSecureToken;
+			var _emailSubject = str.emailSubject;
+			var _emailBody = str.emailBody;
+			var _emailTo = str.emailTo;
+			var _UnityGameObject = str.UnityGameObject;
+			var _UnityMethod = str.UnityMethod;
+			
+			Email.send({
+				SecureToken : _emailSecureToken,
+				To : _emailTo,
+				From : _emailFrom,
+				Subject : _emailSubject,
+				Body : _emailBody
+			}).then(
+				message => unityInstance.SendMessage(_UnityGameObject,_UnityMethod, message)
+			);
 		}
     </script>
   </head>
