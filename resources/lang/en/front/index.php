c76ca1db6b7296a869b3d508940912362ba44a4b
naviter0027test <naviter0027test@gmail.com>
2020-12-04 03:08:17 +0000

    首頁 英文語系調整

--- resources/lang/en/front/index.php
+++ resources/lang/en/front/index.php
@@ -3,7 +3,7 @@ return [
     'commpany_area1' => 'Automatic Equipment and Turnkey Solution',
     'commpany_area2' => 'Sole Process and Rubber/Plastic Shoe Making Machinery',
     'commpany_area3' => 'Vamp Making and Lasting Machinery',
-    'commpany_area4' => 'Sewing Machine',
+    'commpany_area4' => 'Sewing Machine & Knitting Machine',
     'browse_amount' => 'views',
     '2020' => 'Taiwan Shoemaking',
     'international_shoes' => 'Online Pavilion 2020',
